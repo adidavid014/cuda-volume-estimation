9bc2dea8c3ca0cb9dcafa5e57f2ac82a14f78772
adidavid014 <adidavid014@gmail.com>
2024-05-04 16:13:54 -0400

    began working

---
+++ ball_samp-cpu.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    const int D = 16;
+    const int samples = 1000000;
+    const int bins = 100; //0.01, 0.02 --> 0.98, 0.99
+
+}
