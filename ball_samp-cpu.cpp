81b7a66fcde90decc74de4314f795e1ba6f9f2c2
adidavid014 <adidavid014@gmail.com>
2024-05-04 16:15:08 -0400

    added random generator for points and historgram logic

--- ball_samp-cpu.cpp
+++ ball_samp-cpu.cpp
@@ -9,5 +9,16 @@ int main(){
     const int D = 16;
     const int samples = 1000000;
     const int bins = 100; //0.01, 0.02 --> 0.98, 0.99
+    //use random generator for points
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<> dis(-1, 1);
+
+    vector<double> histogram(bins, 0);
+
+    for(int d = 2; d <= D; ++d){
+        fill(histogram.begin(), histogram.end(), 0); //fill with 0s
+        int count = 0;
+    }
 
 }
