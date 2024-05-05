79b158d00bf2546512a80753eb4eb3c455efafc1
adidavid014 <adidavid014@gmail.com>
2024-05-05 14:38:37 -0400

    working on 3-d surface plot with matplot

---
+++ surface_plot.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+#reading data from histogram_data.txt -- output of ball_samp-cpu
+data = np.loadtxt('histogram_data.txt')
+
