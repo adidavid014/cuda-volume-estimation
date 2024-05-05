de7efccbfb888f5333c861741d3f8ddff0a6ae38
adidavid014 <adidavid014@gmail.com>
2024-05-05 14:40:55 -0400

    adding axis for dims and bins

--- surface_plot.py
+++ surface_plot.py
@@ -5,3 +5,7 @@ from mpl_toolkits.mplot3d import Axes3D
 #reading data from histogram_data.txt -- output of ball_samp-cpu
 data = np.loadtxt('histogram_data.txt')
 
+#dimensions and bins
+X = data[:, 0]
+Y = np.arange(1, 101)
+X, Y = np.meshgrid(X, Y)
